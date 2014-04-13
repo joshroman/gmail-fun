ed023f0c9dd588c5b02be5a6c2170f43f10e623f
Josh Roman <joshroman@gmail.com>
2014-04-13 08:29:11 -0400

    Automatic previous date range + config recipient

--- gmail.rb
+++ gmail.rb
@@ -2,34 +2,40 @@
 # https://github.com/nu7hatch/gmail
 
 require 'gmail'
+require 'date'
+require 'active_support/core_ext'
 require './gmail_config'
 
 gmail = Gmail.connect(Gmail_Config::KOKO[:username], Gmail_Config::KOKO[:password])
 
 inbox = gmail.inbox
 expenses = gmail.label("Expenses")
-march_expenses = expenses.find(:before => Date.parse("2014-04-01"), :after => Date.parse("2014-03-01"))
+last_month = Date.today.month - 1
+prev_month_expenses = expenses.find(:before => Date.today.at_beginning_of_month, :after => Date.today.at_beginning_of_month.last_month)
 
 puts "Total number of emails in inbox: #{inbox.count}"
 puts "Total number of unread emails: #{inbox.count(:unread)}"
-puts "All labels: #{gmail.labels.all}"
-puts "Total number of 'Expenses' emails: #{expenses.count}"
-puts "Total number of 'Expenses' emails in March: #{march_expenses.count}"
-march_expenses.each {|email| puts "#{email.date} - #{email.subject}"}
+puts "Total number of 'Expenses' emails last month: #{prev_month_expenses.count}"
+prev_month_expenses.each {|email| puts "#{email.date} - #{email.subject}"}
+#puts "All labels: #{gmail.labels.all}"
+#puts "Total number of 'Expenses' emails: #{expenses.count}"
 
 i = 0
-march_expenses.each do |email|
+prev_month_expenses.each do |email|
+  
   # make this a #forward method...
   new_email = gmail.compose do
-    to "sharon@kokofitclub.com"
+    to "#{Gmail_Config::KOKO[:recipient]}"
     subject "Josh Expenses: #{email.subject}"
     # is there a #message method?
     body "#{email.body}"
     # what about attachments?
   end
+  
   gmail.deliver(new_email)
+  
   i += 1
-  puts "Sent email number #{i} of #{march_expenses.count}"
+  puts "Sent email number #{i} of #{prev_month_expenses.count}"
   
 end
   
