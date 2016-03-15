02f2e2826e1e8c92ec0a07badacdbd2d666c911c
Josh Roman <josh@kokofitclub.com>
2016-03-15 16:51:10 -0400

    Updated gems & config example

--- gmail.rb
+++ gmail.rb
@@ -3,6 +3,7 @@
 
 require 'gmail'
 require 'date'
+require 'active_support'
 require 'active_support/core_ext'
 require './gmail_config'
 
@@ -11,7 +12,8 @@ gmail = Gmail.connect(Gmail_Config::KOKO[:username], Gmail_Config::KOKO[:passwor
 inbox = gmail.inbox
 expenses = gmail.label("Expenses")
 last_month = Date.today.month - 1
-prev_month_expenses = expenses.find(:before => Date.today.at_beginning_of_month, :after => Date.today.at_beginning_of_month.last_month)
+prev_month_expenses = expenses.find(:before => Date.today.beginning_of_month, :after => Date.today.beginning_of_month.last_month)
+
 
 puts "Total number of emails in inbox: #{inbox.count}"
 puts "Total number of unread emails: #{inbox.count(:unread)}"
@@ -22,7 +24,7 @@ prev_month_expenses.each {|email| puts "#{email.date} - #{email.subject}"}
 
 i = 0
 prev_month_expenses.each do |email|
-  
+
   # make this a #forward method...
   new_email = gmail.compose do
     to "#{Gmail_Config::KOKO[:recipient]}"
@@ -31,12 +33,12 @@ prev_month_expenses.each do |email|
     body "#{email.body}"
     # what about attachments?
   end
-  
+
   gmail.deliver(new_email)
-  
+
   i += 1
   puts "Sent email number #{i} of #{prev_month_expenses.count}"
-  
+
 end
-  
+
 gmail.logout
